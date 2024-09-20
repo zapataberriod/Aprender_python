64b27c1b3fe05aeceaf96d72189d116409fc29e8
zapataberriod <91163491+zapataberriod@users.noreply.github.com>
2024-09-20 15:16:46 -0500

    Add files via upload

---
+++ practica.py
@@ -0,0 +1,28 @@
+import math
+
+nombre="Daniel"
+edad=29
+altura=1.55
+humano=True
+print(nombre, edad, altura, humano)
+print ("Hola me llamo " + nombre)
+num1=num2=num3=20
+print(num1)
+apellido ="Zapata"
+print(type(apellido))
+print(len(apellido))
+print(apellido.find("Z"))
+print(apellido.capitalize())
+print(apellido.upper())
+print(apellido.lower())
+print(apellido.isdigit())
+print(apellido.isalpha())
+print(apellido.count("a"))
+print(apellido.replace("a", "o"))
+print("edad", edad)
+protagonista=input("ingresa un nombre")
+edad_protagonista=int(input("ingresa la edad"))
+num=20
+print (abs(num))
+print (pow(num, 2))
+print (math.sqrt(num))
